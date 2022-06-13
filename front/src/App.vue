1accb1ab394c3cc2a07b1db2a44bc35e5e72a392
jeff.dev <johan.06.92@gmail.com>
2022-06-13 23:57:30 +0200

    FEAT: Add font-style for all project

--- front/src/App.vue
+++ front/src/App.vue
@@ -5,3 +5,10 @@
   </div>
   <router-view />
 </template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Playfair+Display:wght@500;600&family=Raleway:wght@400;500&display=swap");
+* {
+  font-family: "Playfair Display", serif;
+}
+</style>
